1439dc78e341c3173c4004a7540dbddc0eb5797c
zlaud <laudkimberly@gmail.com>
2025-04-29 16:06:30 -0700

    upda

--- src/descriptions/events.js
+++ src/descriptions/events.js
@@ -1,48 +1,22 @@
 const events = [
-  {
-    date: "2025-03-20 18:00",
-    title: "SWE Networking Event",
-    description:
-      "Join us for an evening of networking with professionals in the tech industry. Don't miss out on building connections!",
-  },
-  {
-    date: "2025-04-10 17:30",
-    title: "Tech Talk with Industry Experts",
-    description:
-      "Join us for an insightful session with leading industry experts as they share their experiences and expertise in the field.",
-  },
-  {
-    date: "2025-05-15 19:00",
-    title: "Leadership Workshop",
-    description:
-      "A workshop focusing on building leadership skills for aspiring engineers in the STEM field.",
-  },
-
-  {
-    date: "2025-05-15 19:00",
-    title: "Leadership Workshop",
-    description:
-      "A workshop focusing on building leadership skills for aspiring engineers in the STEM field.",
-  },
-  {
-    date: "2025-05-15 19:00",
-    title: "Leadership Workshop",
-    description:
-      "A workshop focusing on building leadership skills for aspiring engineers in the STEM field.",
-  },
-  {
-    date: "2025-05-15 19:00",
-    title: "Leadership Workshop",
-    description:
-      "A workshop focusing on building leadership skills for aspiring engineers in the STEM field.",
-  },
-  {
-    date: "2025-05-15 19:00",
-    title: "Leadership Workshop",
-    description:
-      "A workshop focusing on building leadership skills for aspiring engineers in the STEM field.",
-  },
-
+  // {
+  //   date: "2025-03-20 18:00",
+  //   title: "SWE Networking Event",
+  //   description:
+  //     "Join us for an evening of networking with professionals in the tech industry. Don't miss out on building connections!",
+  // },
+  // {
+  //   date: "2025-04-10 17:30",
+  //   title: "Tech Talk with Industry Experts",
+  //   description:
+  //     "Join us for an insightful session with leading industry experts as they share their experiences and expertise in the field.",
+  // },
+  // {
+  //   date: "2025-05-15 19:00",
+  //   title: "Leadership Workshop",
+  //   description:
+  //     "A workshop focusing on building leadership skills for aspiring engineers in the STEM field.",
+  // },
   // Add more events as needed
 ];
 
